4760519ab4184ebb979c0205506a2f215898209f
SirGlaucus <glaucusjoseph@gmail.com>
2022-11-07 12:59:03 -0300

    Final modulo 4

--- src/App.js
+++ src/App.js
@@ -23,12 +23,18 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("In app.js");
+    console.log(expense);
+  };
+
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expeses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
